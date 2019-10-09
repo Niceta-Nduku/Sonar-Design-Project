54ef7d41db0f00e2d8d9773069ce6977ab7e3179
Humphrey Chiramba <chirambaht@gmail.com>
2019-10-09 11:23:51 +0200

    Final fixed test with all subplots

--- Demo 2/PlanA/test.jl
+++ Demo 2/PlanA/test.jl
@@ -12,7 +12,18 @@ end
 
 while true
 	b = rand(Int, 100) # generate random array
+	c = rand(Int, 100) # generate random array
+	d = rand(Int, 100) # generate random array
+	z = [b,c,d]
 	clf() # this clears the current figure for the next
-	plot(a,b) # this will plot a random array of values
+	subplot(311)
+	plot(a,b)
+	subplot(312)
+	plot(a,c)
+	subplot(313)
+	plot(a,d)
+
+	# plot(a,c) # this will plot a random array of values
+	# plot(a,d) # this will plot a random array of values
 	sleep(1) # this will wait for 1 second
 end
